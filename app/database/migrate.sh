dfec928d4d6a9fc827b661643968d84f5ae3d9eb
balits <balits.peter.marton@gmail.com>
2024-03-11 14:00:38 +0100

    Fix pg_dump script

--- app/database/migrate.sh
+++ app/database/migrate.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-# This file dumps the latest state of the my local (linux) postgres database into an initializer script
-pg_dump -U petiyeti -d elte_konyvtar -s -c -f dump.sql
+#!/bin/sh
+pg_dump -U petiyeti elte_felho_konyvek -s  -C
